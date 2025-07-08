b96014ce02550b495c21d31c940b27818ec8a111
Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>
2025-07-08 16:49:22 +0300

    save
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>

---
+++ install.sh
@@ -0,0 +1,3 @@
+git clone http://git.sysphere.org/awesome-configs ~/.config/awesome/awesome-configs
+cp -r ~/.config/awesome/awesome-configs/scratch ~/.config/awesome
+rm -rf ~/.config/awesome/awesome-configs
